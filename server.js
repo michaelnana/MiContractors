bc516d8ed4b4635bb6b4b5bee359a2fd0df3ca01
michaelnana <zediver>
2017-08-25 01:49:16 -0400

    Changing the port of the server.

--- server.js
+++ server.js
@@ -19,19 +19,5 @@ app.get('/companies/:id', function (req, res) {
   res.sendFile(path.join(views + "company.html"));
 });
 
-/*app.get('/scripts/*', function (req, res) {
-  console.log("Scripts request");
-  res.sendFile(path.join(scripts + file));
-});
-
-app.get('/app/*file', function (req, res) {
-  res.sendFile(path.join(angularApp + file));
-});
-
-app.get('/styles/*file', function (req, res) {
-  res.sendFile(path.join(styles + file));
-});*/
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(80, function () {
 })
